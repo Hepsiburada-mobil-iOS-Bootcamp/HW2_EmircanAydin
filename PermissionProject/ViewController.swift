c68b34ad72f9c6b2ec50c3186b272e053782d366
emircanaydin <emircanaydin@menulux.com>
2021-09-28 00:24:47 +0300

    first commit

---
+++ PermissionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PermissionProject
+//
+//  Created by Emircan Aydın on 28.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
